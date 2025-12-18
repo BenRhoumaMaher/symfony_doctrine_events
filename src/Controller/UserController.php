9fdcf7b215838ab6fc53bae2623dcfdd942f4434
Maher BENRHOUMA <108364709+BenRhoumaMaher@users.noreply.github.com>
2025-12-18 10:33:01 +0100

    Update UserController.php

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -29,7 +29,7 @@ class UserController extends AbstractController
 
         return $this->json(
             [
-            'message' => 'This User was created successfully',
+            'message' => 'The User was created successfully',
             'id' => $user->getId(),
             'email' => $user->getEmail(),
             'stored_password' => $user->getPassword()
